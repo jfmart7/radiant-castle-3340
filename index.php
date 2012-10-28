1295ab989707f98f0ec875b5c7bea8b6f77e9a7c
MBP01 <user@new-host-4.home>
2012-10-28 19:56:08 -0400

    added php stuff

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
+<?php
+/**
+* Copyright 2012 Facebook, Inc.
+*
+* You are hereby granted a non-exclusive, worldwide, royalty-free license to
+* use, copy, modify, and distribute this software in source code or binary
+* form for use in connection with the web services and APIs provided by
+* Facebook.
+*
+* As with any software that integrates with the Facebook platform, your use
+* of this software is subject to the Facebook Developer Principles and
+* Policies [http://developers.facebook.com/policy/]. This copyright notice
+* shall be included in all copies or substantial portions of the software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+
+  require 'server/fb-php-sdk/facebook.php';
+
+  $app_id = 'APP_ID';
+  $app_secret = 'APP_SECRET';
+  $app_namespace = 'test_jfm_dev';
+  $app_url = 'http://apps.facebook.com/' . $app_namespace . '/';
+  $scope = 'email,publish_actions';
+
+  // Init the Facebook SDK
+  $facebook = new Facebook(array(
+    'appId'  => $app_id,
+    'secret' => $app_secret,
+  ));
+// Get the current user
+  $user = $facebook->getUser();
+
+  // If the user has not installed the app, redirect them to the Auth Dialog
+  if (!$user) {
+    $loginUrl = $facebook->getLoginUrl(array(
+      'scope' => $scope,
+      'redirect_uri' => $app_url,
+    ));
+
+    print('<script> top.location.href=\'' . $loginUrl . '\'</script>');
+  }
+
+  if(isset($_REQUEST['request_ids'])) {
+    $requestIDs = explode(',' , $_REQUEST['request_ids']);
+    foreach($requestIDs as $requestID) {
+      try {
+        $delete_success = $facebook->api('/' . $requestID, 'DELETE');
+      } catch(FacebookAPIException $e) {
+        error_log($e);
+      }
+    }
+  }
+?>
+
+<!DOCTYPE html>
 <html>
 <head>
 	<title>JFM Test App</title>
@@ -6,6 +68,6 @@
 	<link href="client/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<p style="color: yellow;">TEST</p>
+<p style="color: yellow;">TEST2</p>
 </body>
 </html>
