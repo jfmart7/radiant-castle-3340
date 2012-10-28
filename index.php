e979695a10d8529a6acfc713d7e1a23bb9b37a8a
MBP01 <user@new-host-4.home>
2012-10-28 19:21:19 -0400

    test page

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <html>
 <head>
+	<title>JFM Test App</title>
+
+	<meta http-equiv="Content-Type" contents="text/html; charset=utf-8" />
+	<link href="client/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <p>TEST</p>
