a9a7f1d42520c0c8208bd23e6e98a663fb5a825f
MBP01 <user@new-host-4.home>
2012-10-28 22:09:03 -0400

    trying to get friends list

--- index.php
+++ index.php
@@ -103,17 +103,17 @@
     FB.getLoginStatus(function(response) {
       uid = response.authResponse.userID ? response.authResponse.userID : null;
       document.write('<p style="color=red;">UID: ' + uid + '</p><br/>');
-
-   // FB.api('/me/friends?limit=5', function(response) {
-//	if(response.data) {
-//		$.each(response.data,function(index,friend) {
-//			document.write('<p>' + friend.name + ' : ' + friend.id + '</p><br/>');
-//		});
-//	} else {
-//		document.write('<p>Couldn't get friends list</p><br/>');
-//	}
-//   });
     });
+    FB.api('/me/friends', function(response) {
+	if(response.data) {
+		
+		$.each(response.data,function(index,friend) {
+			document.write('<p>' + friend.name + ' : ' + friend.id + '</p><br/>');
+		});
+	} else {
+		document.write('<p>Couldn't get friends list</p><br/>');
+	}
+   });
   </script> 
 
 </body>
